bce13ff3b4b14615f4afc19d04c3c77b414a56c8
Salman Ahsan <salmanahsan@iut-dhaka.edu>
2023-02-02 12:19:00 +0600

    fixed some bugs

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const app = express();
 
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 
-app.listen(Process.env.PORT || PORT, ()=>{
-    console.log("Server running at port: ", PORT)
+app.listen(PORT, ()=>{
+    console.log(`Server running at port: ${PORT}`)
 })
