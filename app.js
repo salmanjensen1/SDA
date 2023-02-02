1097e5bf561582105ef5291548bbaa11f723d269
Salman Ahsan <salmanahsan@iut-dhaka.edu>
2023-02-02 12:23:23 +0600

    added homepage

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 
 const app = express();
+const cors = require('cors')
 
 const PORT = process.env.PORT || 1337;
+
+app.get('/', (req, res)=>{
+    res.json("Harder than getting on top of Everest")
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server running at port: ${PORT}`)
